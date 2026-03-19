8fb8d6d4eda30476354bad1119d22dc5406ab627
vinh16112004-cloud <vinh16112004@gmail.com>
2026-03-20 01:53:13 +0700

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -1,29 +1,28 @@
-// Tìm đến phần return trong file Register.jsx và đảm bảo có cấu trúc:
-<div class="register_container" style={{width: "50%", margin: "auto"}}>
-    <div class="header" style={{display: "flex",flexDirection: "row", justifyContent: "space-between"}}>
-        <span class="text" style={{flexGrow: "1"}}>SignUp</span> 
+<div className="register_container" style={{width: "50%", margin: "auto"}}>
+    <div className="header" style={{display: "flex",flexDirection: "row", justifyContent: "space-between"}}>
+        <span className="text" style={{flexGrow: "1"}}>SignUp</span> 
     </div>
     <hr/>
     <form onSubmit={register}>
-        <div class="inputs">
-            <div class="input">
+        <div className="inputs">
+            <div className="input">
               <input type="text" name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}/>
             </div>
-            <div class="input">
+            <div className="input">
               <input type="text" name="first_name" placeholder="First Name" className="input_field" onChange={(e) => setFirstName(e.target.value)}/>
             </div>
-            <div class="input">
+            <div className="input">
               <input type="text" name="last_name" placeholder="Last Name" className="input_field" onChange={(e) => setLastName(e.target.value)}/>
             </div>
-            <div class="input">
+            <div className="input">
               <input type="email" name="email" placeholder="Email" className="input_field" onChange={(e) => setEmail(e.target.value)}/>
             </div>
-            <div class="input">
+            <div className="input">
               <input type="password" name="password" placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>
             </div>
         </div>
-        <div class="submit_container">
-            <button type="submit" class="status">Register</button>
+        <div className="submit_container">
+            <button type="submit" className="status">Register</button>
         </div>
     </form>
 </div>
